627a0e770059b68f883ea0774dfe433852d80d38
elias-hannoun <eliashan858@gmail.com>
2025-02-01 18:23:03 -0500

    Adding a second page (home). + adding a new button

--- app/index.jsx
+++ app/index.jsx
@@ -4,13 +4,15 @@ import { useRouter } from 'expo-router'
 import ScreenWrapper from '../components/ScreenWrapper'
 import Button from '../components/Button'
 import Loading from '../components/Loading'
+import {theme} from "../constants/theme";
 
 const index = () => {
     const router = useRouter();
   return (
     <ScreenWrapper>
       <Text>index File</Text>
-      <Button title='Welcome' onPress={() => router.push('welcome')} />
+      <Button title='Welcome' onPress={() => router.push('/home')} />
+        <Button title='Welcome 2' buttonStyle={{ backgroundColor: 'blue', margin: 20 , height: 50, width: '80%', alignSelf: 'center' }}/>
       {/*<Loading/>*/}
     </ScreenWrapper>
   )
