38485d195ed19203c8648603b6889da6cced3bd6
RawdaWaez <rawdawaez@gmail.com>
2025-02-02 12:39:01 -0500

    Fixed Merge Problem

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
 import ScreenWrapper from '../components/ScreenWrapper'
-import Button from '../components/Button'
-import Loading from '../components/Loading'
-import {theme} from "../constants/theme";
 
 const index = () => {
     const router = useRouter();
-  return (
-    <ScreenWrapper>
-      <Text>index File</Text>
-      <Button title='Welcome' onPress={() => router.push('/home')} />
-        <Button title='Welcome 2' buttonStyle={{ backgroundColor: 'blue', margin: 20 , height: 50, width: '80%', alignSelf: 'center' }}/>
-      {/*<Loading/>*/}
-    </ScreenWrapper>
-  )
+    return (
+        <ScreenWrapper>
+            <Text>index File</Text>
+            <Button title='Welcome' onPress={() => router.push('welcome')} />
+        </ScreenWrapper>
+    )
 }
 
 export default index
