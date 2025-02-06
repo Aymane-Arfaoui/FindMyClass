be5551dc47ccb9fa9e0af41a5a278df6eaf38cd6
ctmoon20 <123218005+ctmoon20@users.noreply.github.com>
2025-02-06 18:39:28 -0500

    main page logo addition

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,30 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Image} from 'react-native';
+import React, { useEffect } from 'react'
 import { useRouter } from 'expo-router'
 import ScreenWrapper from '../components/ScreenWrapper'
+import whiteLogo from '../assets/images/logo_whitepage.png'
+import styling from '../assets/Styles/mainPageStyle.js'
 
 const index = () => {
     const router = useRouter();
+
+    // This is a timer that shows the index page for 3 seconds before going to the welcome page
+    // Can change this later if need less or more time
+    useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('Welcome');
+    }, 3000);
+    return () => clearTimeout(timer);
+    }, [router]);
+    
   return (
     <ScreenWrapper>
-      <Text>index File</Text>
-      <Button title='Welcome' onPress={() => router.push('Welcome')} />
+      <View style={styling.container}>
+        <Image source={whiteLogo} style={styling.logo} />
+        {/* <Button title="Welcome" onPress={() => router.push('Welcome')} /> */}
+      </View>
     </ScreenWrapper>
   )
 }
 
-export default index
+export default index;
