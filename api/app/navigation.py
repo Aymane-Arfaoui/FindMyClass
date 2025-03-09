09a285b1c9a95e8a0ee108daf592b1eec98aa0f5
M-a-a-d-man <evanteboul@icloud.com>
2025-03-08 20:05:27 -0500

    graph routing partially complete

--- api/app/navigation.py
+++ api/app/navigation.py
@@ -1,20 +1,25 @@
+import string
 from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
+import os
 
 import graph.Graph as Graph
 navigation_routes = Blueprint('indoor_navigation', __name__)
 
 g = None
-import os
+
 
 @navigation_routes.route('/indoor_navigation', methods=['GET'])
 @cross_origin()
-def indoor_navigation():
+def indoor_navigation(startId:string, endId:string):
     if g is None:
+        for root, dirs, files in os.walk('api/app/data'):
+            for file in files:
+                if file.endswith('.json'):
+                    g = Graph.load_graph_from_json(g, os.path.join(root, file))
+        
 
-        g = Graph.load_graph_from_json(None, 'api/app/graph/graph.json')
-
-    return jsonify(h_building_first_floor)
+    g.
 
 
 
