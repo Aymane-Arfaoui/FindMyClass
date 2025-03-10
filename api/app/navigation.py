6f06cc18afe17f29d68853054e93800ec2a22699
M-a-a-d-man <evanteboul@icloud.com>
2025-03-09 20:01:32 -0400

    added default graphs

--- api/app/navigation.py
+++ api/app/navigation.py
@@ -2,7 +2,7 @@ import os
 from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
 import app.graph.Graph as Graph
-
+from collections import defaultdict
 
 navigation_routes = Blueprint('indoorNavigation', __name__)
 
@@ -11,7 +11,7 @@ g = {}
 @navigation_routes.route('/indoorNavigation', methods=['GET'])
 @cross_origin()
 def indoor_navigation():
-    global g
+    
     start_id = request.args.get('startId')
     end_id = request.args.get('endId')
     campus = request.args.get('campus')
@@ -22,8 +22,8 @@ def indoor_navigation():
     if not start_id or not end_id:
         return jsonify({"error": "Missing required parameters 'startId' and 'endId'"}), 400
 
-    if g == {} or campus not in g:
-        g[campus] = None
+    if campus not in g:
+        g[campus] = Graph.Graph()
         for root, dirs, files in os.walk(f'app/data/campus_jsons/{campus}'):
             for file in files:
                 if file.endswith('.json'):
@@ -31,4 +31,7 @@ def indoor_navigation():
 
     path = g[campus].find_shortest_path(start_id, end_id)
 
-    return jsonify({"path": path})
+    if not path:
+        return jsonify({"error": "Destination inaccessible from Start location"}), 204
+
+    return jsonify({"path": path}), 200
