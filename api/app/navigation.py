c2bfda9f3943aa7f4e83b385893a2e81251b3664
M-a-a-d-man <evanteboul@icloud.com>
2025-03-09 22:39:29 -0400

    changed graph test to pytest

--- api/app/navigation.py
+++ api/app/navigation.py
@@ -1,7 +1,7 @@
 import os
 from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
-from app.graph.Graph import Graph
+from app.graph.Graph2 import Graph
 
 # import app.graph.Graph as Graph
 # from collections import defaultdict
