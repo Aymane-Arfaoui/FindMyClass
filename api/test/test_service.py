7396936b53c95d0c0290bec206b7d8840f7d10a7
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2025-03-12 18:57:45 -0400

    changed file name and added test

--- api/test/test_service.py
+++ api/test/test_service.py
@@ -1,5 +1,3 @@
-# api/test/test_service.py
-
 import pytest
 import json
 from api.app.service import app
@@ -7,28 +5,26 @@ import requests
 from requests.models import Response
 from unittest.mock import patch
 
-
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-
-def mock_requests_get(*args, **kwargs):
+def mock_requests_get_success(*args, **kwargs):
     mock_response = Response()
     mock_response.status_code = 200
     mock_response._content = b'{"buildings": [{"id": 1, "name": "Building 1"}]}'
     return mock_response
 
-
-@patch('requests.get', side_effect=mock_requests_get)
-def test_get_building_list(mock_get, client):
+@patch('requests.get', side_effect=mock_requests_get_success)
+def test_get_building_list_success(mock_get, client):
     response = client.get('/api/buildinglist')
     assert response.status_code == 200
     data = json.loads(response.data)
     assert 'buildings' in data
     assert len(data['buildings']) == 1
     assert data['buildings'][0]['name'] == 'Building 1'
+
 
 def mock_failed_requests_get(*args, **kwargs):
     mock_response = Response()
@@ -37,9 +33,9 @@ def mock_failed_requests_get(*args, **kwargs):
     return mock_response
 
 @patch('requests.get', side_effect=mock_failed_requests_get)
-def test_get_building_list_failure(mock_get, client):
+def test_get_building_list_api_error(mock_get, client):
     response = client.get('/api/buildinglist')
     assert response.status_code == 500
     data = json.loads(response.data)
     assert 'error' in data
-    assert data['error'] == 'Internal Server Error'  # Adjusted this line to match the Flask error message
+    assert data['error'] == 'Internal Server Error'
