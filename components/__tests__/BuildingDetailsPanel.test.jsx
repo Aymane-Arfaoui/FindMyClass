3227344051674ff4ec4b9858d15c82dc80222778
AbdelmalekMouhamou <146683074+AbdelmalekMouhamou@users.noreply.github.com>
2025-03-16 18:43:20 -0400

    refactored buildingDetailsPanel.test.jsx

--- components/__tests__/BuildingDetailsPanel.test.jsx
+++ components/__tests__/BuildingDetailsPanel.test.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { render, userEvent, screen } from '@testing-library/react-native';
 import BuildingDetailsPanel from '../BuildingDetailsPanel';
 import { useNavigation } from '@react-navigation/native';
-describe('<BuildingDetailsPanel />', () => {
+
+describe('BuildingDetailsPanel', () => {
     const mockBuildingDetails = {
         formatted_address: "123 Main St, City, Country",
         photos: [{ photo_reference: '123abc' }],
@@ -11,11 +12,24 @@ describe('<BuildingDetailsPanel />', () => {
     const mockOnDirectionPress = jest.fn();
     const mockNavigate =jest.fn();
 
-    const selectedBuilding = { name: "Hall Building" };
     const currentLocation = { geometry: { coordinates: [1,2] } };
     const mode = 'driving';
     const GOOGLE_PLACES_API_KEY = "test";
-    const loading = false;
+    let buildingDetailsComponent=(selectedBuilding,loading)=>{
+        return (<BuildingDetailsPanel
+            selectedBuilding={selectedBuilding}
+            buildingDetails={{ ...mockBuildingDetails, photos: [] }}
+            panHandlers={{}} // mock panHandlers if needed
+            panelY={0}
+            onClose={mockOnClose}
+            onDirectionPress={mockOnDirectionPress}
+            currentLocation={currentLocation}
+            mode={mode}
+            GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
+            loading={loading}
+        />);
+    }
+
     beforeAll(()=>{
         useNavigation.mockReturnValue({
             navigate:mockNavigate,
@@ -23,20 +37,7 @@ describe('<BuildingDetailsPanel />', () => {
         })})
 
     it('should render building name and address', () => {
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={selectedBuilding}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}}
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({ name: "Hall Building" },false));
 
 
         expect(screen.getByText('Hall Building')).toBeTruthy();
@@ -44,122 +45,43 @@ describe('<BuildingDetailsPanel />', () => {
     });
 
     it('should show loading indicator when loading is true', () => {
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={selectedBuilding}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}}
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={true}
-            />
-        );
+        render(buildingDetailsComponent({ name: "Hall Building" },true));
+
         expect(screen.getByTestId('loading-indicator')).toBeTruthy();
     });
 
     it('should trigger onClose when close button is pressed', async () => {
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={selectedBuilding}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}} // mock panHandlers if needed
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({ name: "Hall Building" },false));
         const user = userEvent.setup()
         await user.press(screen.getByTestId('close-button'));
         expect(mockOnClose).toHaveBeenCalled();
     });
 
     it('should call onDirectionPress when directions button is pressed', async () => {
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={selectedBuilding}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}} // mock panHandlers if needed
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({ name: "Hall Building" },false));
 
         const user = userEvent.setup()
         await user.press(screen.getByTestId('direction-button'));
-        expect(mockOnDirectionPress).toHaveBeenCalledWith(currentLocation, selectedBuilding, mode);
+        expect(mockOnDirectionPress).toHaveBeenCalledWith(currentLocation, { name: "Hall Building" }, mode);
     });
 
     it('should navigate to "MapScreen" with key hall when indoor map button is pressed and hall is selected', async () => {
 
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={selectedBuilding}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}} // mock panHandlers if needed
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({ name: "Hall Building" },false));
 
         const user = userEvent.setup()
         await user.press(screen.getByTestId('indoor-map-button'));
         expect(mockNavigate).toHaveBeenCalledWith('MapScreen', {buildingKey: 'Hall'});
     });
     it('should navigate to "MapScreen" with key CC when indoor map button is pressed and CC is selected', async () => {
-
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={{name: "Central Building"}}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}}
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({name: "Central Building"},false));
 
         const user = userEvent.setup()
         await user.press(screen.getByTestId('indoor-map-button'));
         expect(mockNavigate).toHaveBeenCalledWith('MapScreen', {buildingKey: 'CC'});
     });
     it('should navigate to "MapScreen" with key MB when indoor map button is pressed and MB is selected', async () => {
-
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={{name: "John Molson"}}
-                buildingDetails={mockBuildingDetails}
-                panHandlers={{}}
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({name: "John Molson"},false));
 
         const user = userEvent.setup()
         await user.press(screen.getByTestId('indoor-map-button'));
@@ -167,20 +89,7 @@ describe('<BuildingDetailsPanel />', () => {
     });
 
     it('should use default image if no photo reference is available', () => {
-        render(
-            <BuildingDetailsPanel
-                selectedBuilding={selectedBuilding}
-                buildingDetails={{ ...mockBuildingDetails, photos: [] }}
-                panHandlers={{}} // mock panHandlers if needed
-                panelY={0}
-                onClose={mockOnClose}
-                onDirectionPress={mockOnDirectionPress}
-                currentLocation={currentLocation}
-                mode={mode}
-                GOOGLE_PLACES_API_KEY={GOOGLE_PLACES_API_KEY}
-                loading={loading}
-            />
-        );
+        render(buildingDetailsComponent({ name: "Hall Building" },false));
 
         const defaultImage = screen.getByTestId('default-image');
         expect(defaultImage.props.source.uri).toBe("https://www.kpmb.com/wp-content/uploads/2016/06/0004_N76_300dpi-scaled.jpg");
