51e275ae63be366c118776ccfe4bed01d0ceeb89
RawdaWaez <rawdawaez@gmail.com>
2025-04-05 00:33:48 -0400

    fix minor

--- app/chat.jsx
+++ app/chat.jsx
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { ThemeContext } from '@/context/ThemeProvider';
-import ChatInterface from '@/components/ChatInterface';
-import { useRouter } from 'expo-router';
-
-export default function ChatScreen() {
-  const { isDark } = useContext(ThemeContext);
-  const router = useRouter();
-  
-  return (
-    <>
-      <StatusBar style={isDark ? 'light' : 'dark'} />
-      <ChatInterface navigation={{ goBack: () => router.back() }} />
-    </>
-  );
-} 
+// import React, { useContext } from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { ThemeContext } from '@/context/ThemeProvider';
+// import ChatInterface from '@/components/ChatInterface';
+// import { useRouter } from 'expo-router';
+//
+// export default function ChatScreen() {
+//   const { isDark } = useContext(ThemeContext);
+//   const router = useRouter();
+//
+//   return (
+//     <>
+//       <StatusBar style={isDark ? 'light' : 'dark'} />
+//       <ChatInterface navigation={{ goBack: () => router.back() }} />
+//     </>
+//   );
+// }
